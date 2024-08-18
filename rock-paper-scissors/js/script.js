2d1e1115fb1eea11944dabd2e6a8a916653a93c7
Ronnie Chan <ronniekkc@gmail.com>
2024-08-18 17:17:53 -0400

    feat: Add game logic and event handlers

--- rock-paper-scissors/js/script.js
+++ rock-paper-scissors/js/script.js
@@ -24,3 +24,104 @@ let humanScore;
 let computerScore;
 let rounds;
 let endOfGame;
+
+// --------------------- //
+// HELPER FUNCTIONS
+
+const displayRoundDetails = function (currentOutcome, winnerMove, loserMove) {
+  roundOutcomeEl.textContent = `${currentOutcome} in round ${rounds}!`;
+  roundResultEl.textContent = `${winnerMove} ${winnerMove === loserMove ? 'equals' : 'beats'} ${loserMove}. `;
+};
+
+const clearSetups = function () {
+  humanChoiceEl.classList.remove('rock', 'paper', 'scissors');
+  computerChoiceEl.classList.remove('rock', 'paper', 'scissors');
+  document.querySelector('.player-human .win-marker').classList.add('removed');
+  document.querySelector('.player-computer .win-marker').classList.add('removed');
+  document.querySelector(`.player-human`).classList.remove('winner-setup');
+  document.querySelector(`.player-computer`).classList.remove('winner-setup');
+};
+
+// --------------------- //
+// GAME LOGIC FUNCTIONS
+
+// Start or reset the game
+const init = function () {
+  humanScore = 0;
+  computerScore = 0;
+  rounds = 1;
+  endOfGame = false;
+
+  humanScoreEl.textContent = humanScore;
+  computerScoreEl.textContent = computerScore;
+  gamePhaseEl.textContent = 'vs';
+  roundOutcomeEl.textContent = 'Choose your move';
+  roundResultEl.textContent = '...';
+
+  clearSetups();
+  movesEl.classList.remove('hidden');
+};
+
+const getComputerChoice = function () {
+  const randomChoice = Math.trunc(Math.random() * 3);
+  const rockPaperScissors = ['Rock', 'Paper', 'Scissors'];
+  return rockPaperScissors[randomChoice];
+};
+
+const playRound = function (humanChoice, computerChoice) {
+  // Display selected moves
+  gamePhaseEl.textContent = `Round ${rounds}`;
+  humanChoiceEl.classList.add(`${humanChoice.toLowerCase()}`);
+  computerChoiceEl.classList.add(`${computerChoice.toLowerCase()}`);
+
+  // When there is a draw
+  if (humanChoice === computerChoice) {
+    displayRoundDetails('A draw', humanChoice, computerChoice);
+    return;
+  }
+
+  const humanWins = (humanChoice === 'Rock' && computerChoice === 'Scissors') || (humanChoice === 'Paper' && computerChoice === 'Rock') || (humanChoice === 'Scissors' && computerChoice === 'Paper');
+
+  // When human wins
+  if (humanWins) {
+    humanScore++;
+    humanScoreEl.textContent = humanScore;
+    displayRoundDetails('You win', humanChoice, computerChoice);
+    document.querySelector('.player-human .win-marker').classList.remove('removed');
+
+    // When computer wins
+  } else {
+    computerScore++;
+    computerScoreEl.textContent = computerScore;
+    displayRoundDetails('You lose', computerChoice, humanChoice);
+    document.querySelector('.player-computer .win-marker').classList.remove('removed');
+  }
+
+  // Increment rounds
+  rounds++;
+  endOfGame = rounds > 5;
+};
+
+const playGame = function (humanChoice) {
+  // Play the game for 5 rounds
+  clearSetups();
+  const computerChoice = getComputerChoice();
+  playRound(humanChoice, computerChoice);
+
+  // Announce the champion after round 5
+  if (endOfGame) {
+    let champion = humanScore > computerScore ? 'human' : 'computer';
+    document.querySelector(`.player-${champion}`).classList.add('winner-setup');
+    movesEl.classList.add('hidden');
+  }
+};
+
+// --------------------- //
+// EVENT HANDLERS
+
+init();
+
+btnRock.addEventListener('click', () => playGame('Rock'));
+btnPaper.addEventListener('click', () => playGame('Paper'));
+btnScissors.addEventListener('click', () => playGame('Scissors'));
+btnReset.addEventListener('click', init);
