7efd3b08e7b8aed5ae982514d113d4ab31c2b791
Ronnie Chan <ronniekkc@gmail.com>
2024-08-18 17:16:21 -0400

    chore: Define DOM elements and state variables for game setup

--- rock-paper-scissors/js/script.js
+++ rock-paper-scissors/js/script.js
@@ -1,1 +1,26 @@
 'use script';
+
+// --------------------- //
+// SELECT DOM ELEMENTS
+
+const btnRock = document.querySelector('.btn-rock');
+const btnPaper = document.querySelector('.btn-paper');
+const btnScissors = document.querySelector('.btn-scissors');
+const btnReset = document.querySelector('.btn-reset');
+
+let humanScoreEl = document.querySelector('.score-human');
+let computerScoreEl = document.querySelector('.score-computer');
+let humanChoiceEl = document.querySelector('.choice-human');
+let computerChoiceEl = document.querySelector('.choice-computer');
+let roundOutcomeEl = document.querySelector('.round-outcome');
+let roundResultEl = document.querySelector('.round-result');
+let gamePhaseEl = document.querySelector('.game-phase');
+let movesEl = document.querySelector('.moves');
+
+// --------------------- //
+// STATE VARIABLES
+
+let humanScore;
+let computerScore;
+let rounds;
+let endOfGame;
