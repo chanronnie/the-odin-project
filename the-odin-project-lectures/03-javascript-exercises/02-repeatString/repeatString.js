68513f9c261d1b5365485ef85dfec12ce2ba0b0b
Ronnie Chan <ronniekkc@gmail.com>
2024-08-19 17:02:21 -0400

    chore: Completed JS ex 2

---
+++ the-odin-project-lectures/03-javascript-exercises/02-repeatString/repeatString.js
@@ -0,0 +1,12 @@
+const repeatString = function (string, times) {
+  if (times <= 0) return "Invalid argument for times";
+
+  let result = "";
+  for (let i = 0; i < times; i++) {
+    result += string;
+  }
+  return result;
+};
+
+// Do not edit below this line
+module.exports = repeatString;
