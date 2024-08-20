85f9f7dd277f005d6bd99e6af213df5eca2de592
Ronnie Chan <ronniekkc@gmail.com>
2024-08-20 13:32:36 -0400

    chore: Add comment to clarify JS ex 4

--- the-odin-project-lectures/03-javascript-exercises/04-removeFromArray/removeFromArray.js
+++ the-odin-project-lectures/03-javascript-exercises/04-removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
 const removeFromArray = function (array, ...args) {
+  // Rest Parameters: Syntax that allows the function to accept a list of an infinite number of arguments [...args]
+  // Filter function: filter out elements that satisfies the condition
   return array.filter((element) => !args.includes(element));
 };
 
