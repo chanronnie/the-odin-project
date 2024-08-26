697af9761bd935a9aa5a9cec71713de3c8dd4ad8
Ronnie Chan <ronniekkc@gmail.com>
2024-08-26 06:44:05 -0400

    chore: Define DOM elements and state variables for app setup

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -1,3 +1,21 @@
 // CALCULATOR 2024 - Author: Ronnie Chan
 
 "use strict";
+
+// -------------------------- //
+// SELECT DOM ELEMENTS
+
+let displayInputEl = document.querySelector(".display-input");
+let displayResultEl = document.querySelector(".display-result");
+const btnClear = document.querySelector(`.btn-clear`);
+const btnSign = document.querySelector(".btn-sign");
+const btnPercent = document.querySelector(".btn-percent");
+
+// -------------------------- //
+// VARIABLES
+let currNum;
+let firstNum;
+let secondNum;
+let operator;
+let hasError;
+let hasCalculated;
